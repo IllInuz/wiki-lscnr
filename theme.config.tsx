453e58e5ee21697d9fe0190283f605ca9dd7a1ab
Illinuz <snellemany@gmail.com>
2025-02-07 19:57:47 -0600

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Lscnr</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/IllInuz/wiki-lscnr',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/IllInuz/wiki-lscnr',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2024 LSCNR - All rights reserved.',
   },
 }
 
